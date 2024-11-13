f0e676f8709d1d334c621fec1459e0a263bf04a9
Blaize <blaizedevap@gmail.com>
2024-11-13 20:15:26 +0100

    the js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+$(document).ready(function(){
+
+    $('.sidenav').sidenav();
+});
+
+
+
+
+$(document).ready(function(){
+
+    $("#le").click(function(){$(this).hide();});
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        indicators:true
+    });
+
+    autoplay();
+    function autoplay(){
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 10000);
+    }
+})
